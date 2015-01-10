05d8690510b7415f1c1d4e69771f3afaad33138b
Ryan Drapeau <drapeau@cs.washington.edu>
2015-01-10 13:40:25 -0800

    Config File

--- swiper.py
+++ swiper.py
@@ -1,39 +1,65 @@
 import requests, json, multiprocessing
 
-facebook_token = '' #get this from the api explorer or something
-facebook_id = ''
+config = open('config.txt', 'r').read().split('\n')
+FB_ID = config[0]
+FB_TOKEN = config[1]
+LOGIN_CRED = {'facebook_token' : FB_TOKEN, 'facebook_id' : FB_ID}
+MIN_FRIEND_COUNT = 15
 
-loginCredentials = {'facebook_token':facebook_token, 'facebook_id' : facebook_id}
-headers = {'Content-Type' : 'application/json', 'User-Agent' : 'Tinder Android Version 3.2.0'}
-
-r = requests.post('https://api.gotinder.com/auth', data=json.dumps(loginCredentials), headers=headers)
-x_auth_token = r.json()['token']
-
-
-def process_girl(girl):
-    _id = girl['_id']
+# Like a single girl and print out her name
+def process_girl(girl, auth_token):
+    tinder_id = girl['_id']
     name = girl['name']
-    headers3 = {'X-Auth-Token' : x_auth_token, 'User-Agent' : 'Tinder Android Version 3.2.0'}
-    r3 = requests.get('https://api.gotinder.com/like/' + _id, headers=headers3)
-    print name, ":", girl['bio'].replace("\n", " ")
+    bio = girl['bio'].replace("\n", " ") # Remove new lines
+    friend_count = girl['common_friend_count']
+    headers = {'X-Auth-Token' : auth_token, 'User-Agent' : 'Tinder Android Version 3.2.0'}
+    request = requests.get('https://api.gotinder.com/like/' + tinder_id, headers = headers)
 
+    if friend_count > MIN_FRIEND_COUNT:
+        # Friends
+        print
+        print 'Common Friend', name, '(' + str(friend_count) + '):', bio
+        print
+    elif 'snap' in bio.lower():
+        # Snapchat
+        print name, ':', bio
+    else:
+        print name
 
-count = 0
-while True:
-    headers2 = {'User-Agent' : 'Tinder Android Version 3.2.0', 'Content-Type' : 'application/json', 'X-Auth-Token' : x_auth_token}
-    r2 = requests.get('https://api.gotinder.com/user/recs', headers=headers2)
-    girls = r2.json()['results']
+def main():
+    # Authenticate
+    headers = {'Content-Type' : 'application/json', 'User-Agent' : 'Tinder Android Version 3.2.0'}
+    request = requests.post('https://api.gotinder.com/auth', data = json.dumps(LOGIN_CRED), headers = headers)
 
-    processes = []
-    for girl in girls:
-        process = multiprocessing.Process(target = process_girl, args = (girl,))
-        processes.append(process)
-        process.start()
+    if 'token' not in request.json():
+        print 'Please Refresh FB Token'
+        return
 
-    for process in processes:
-        process.join()
+    auth_token = request.json()['token']
 
-    count += len(processes)
+    # Grab first 10
+    liked = set()
+    headers = {'User-Agent' : 'Tinder Android Version 3.2.0', 'Content-Type' : 'application/json', 'X-Auth-Token' : auth_token}
+    request = requests.get('https://api.gotinder.com/user/recs', headers = headers)
 
-    print count, "Girls Liked"
+    # Continue while there are more
+    while 'results' in request.json():
+        girls = request.json()['results']
+        processes = []
 
+        for girl in girls:
+            liked.add(girl['_id'])
+            process = multiprocessing.Process(target = process_girl, args = (girl, auth_token))
+            processes.append(process)
+            process.start()
+
+        for process in processes:
+            process.join()
+
+        # Grab next 10
+        request = requests.get('https://api.gotinder.com/user/recs', headers = headers)
+
+    print len(liked), "Girls Liked"
+
+if __name__ == "__main__":
+    main()
